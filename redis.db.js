66b38e835a9b1c676a2adc812489360f17da6124
pooooh14 <monisha1497@gmail.com>
2023-11-09 15:07:41 +0530

    make changes in redis.js

--- redis.db.js
+++ redis.db.js
@@ -1,7 +1,19 @@
 let Redis = require('ioredis')
 require('dotenv').config()
-let redis = new Redis(`${process.env.redis}`)
+let redis = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  });
 
+  // Check if Redis is connected
+redis.on('connect', () => {
+    console.log('Connected to Redis');
+});
+  
+  // Check for errors
+redis.on('error', (err) => {
+    console.error('Error connecting to Redis:', err);
+});
 console.log("redis")
 
 module.exports = {redis}
